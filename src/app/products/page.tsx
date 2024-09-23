74ec18c9faf962fbc681fdbbaa4cf92632b0f18d
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-09-23 18:30:45 -0300

    chore: create infinity pagination

---
+++ src/app/products/page.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { useInfiniteQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import Link from "next/link";
+import { Fragment, useEffect } from "react";
+import { FaCartArrowDown } from "react-icons/fa";
+import { useInView } from "react-intersection-observer";
+
+import Loading, { LoadingIcon } from "@/components/Loading";
+import getAllProducts from "@/service/queries";
+
+export default function Products() {
+  const { data, isLoading, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    queryFn: getAllProducts,
+    queryKey: ["products"],
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+  });
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-blue-100 p-8">
+      <h1 className="text-4xl font-extrabold text-center text-blue-800 mb-10">
+        Explore Our Products
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {data?.pages.map((page) => (
+          <Fragment key={page.currentPage}>
+            {page.data.map((product) => (
+              <div
+                key={product.id}
+                className="group relative p-6 flex gap-6 flex-col bg-white rounded-xl shadow-lg hover:shadow-2xl transition duration-300 ease-in-out transform hover:-translate-y-2 hover:scale-105 border border-gray-200"
+              >
+                <div className="mb-4">
+                  <h2 className="text-2xl font-bold text-gray-800 group-hover:text-blue-700 transition">
+                    {product.title}
+                  </h2>
+
+                  <p className="text-lg text-gray-600">${product.price}</p>
+                  <p className="text-sm text-gray-500">{product.category}</p>
+                </div>
+                <Link
+                  href={`/product/${product.id}`}
+                  className="flex h-full justify-center items-center overflow-hidden"
+                >
+                  <Image
+                    src={product.image}
+                    alt={product.title}
+                    width={300}
+                    height={250}
+                    className="rounded-lg w-auto h-auto object-cover group-hover:scale-110 transition-transform duration-300"
+                  />
+                </Link>
+                <div className="flex flex-col items-center justify-center gap-2">
+                  <button className="text-blue-500 text-lg hover:text-blue-400 active:text-blue-600 ease-in-out duration-200">
+                    Buy Now
+                  </button>
+                  <button className="">
+                    <FaCartArrowDown
+                      className="inline-block text-center text-blue-500 active:text-blue-300 ease-in-out duration-200"
+                      size={32}
+                    />
+                  </button>
+                </div>
+              </div>
+            ))}
+          </Fragment>
+        ))}
+      </div>
+      {hasNextPage && (
+        <div ref={ref} className="flex justify-center mt-8">
+          <LoadingIcon />
+        </div>
+      )}
+    </div>
+  );
+}
--- src/service/queries/index.ts
+++ src/service/queries/index.ts
@@ -2,8 +2,24 @@ import { Product, Products } from "@/types/Products";
 
 import { fetcher } from "../fetcher/axiosInstance";
 
-export async function getAllProducts(): Promise<Products> {
-  return fetcher("/products");
+export default async function getAllProducts({ pageParam = 0 }): Promise<{
+  data: Products;
+  currentPage: number;
+  nextPage: number | undefined;
+}> {
+  const limit = 5;
+  const products: Products = await fetcher(`/products`);
+
+  const productsLimited: Products = products.slice(
+    pageParam * limit,
+    (pageParam + 1) * limit,
+  );
+
+  return {
+    data: productsLimited,
+    currentPage: pageParam,
+    nextPage: productsLimited.length === limit ? pageParam + 1 : undefined,
+  };
 }
 
 export async function getProductById(id: number): Promise<Product> {
