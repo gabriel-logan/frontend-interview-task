d6c87a03e3bc247d1e8bd27e4247e1f022259b9e
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-09-24 14:54:44 -0300

    fix: home page button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,14 @@ export default function Home() {
       <motion.button
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.9 }}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="text-white font-bold"
       >
-        <Link href="/products">See Products</Link>
+        <Link
+          href="/products"
+          className="p-4 rounded bg-blue-500 hover:bg-blue-700"
+        >
+          See Products
+        </Link>
       </motion.button>
     </motion.div>
   );
