b05e95c1eb653be2cb6eaca51c33db6bbdc8806a
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-09-24 14:50:50 -0300

    Adjusting latest styles

--- src/components/HeaderProducts.tsx
+++ src/components/HeaderProducts.tsx
@@ -2,10 +2,11 @@
 
 import { MdOutlineShoppingCart } from "react-icons/md";
 
-import { useCartModal } from "@/zustand/store";
+import { useCart, useCartModal } from "@/zustand/store";
 
 export default function HeaderProducts() {
   const { setCartModal, cartModal } = useCartModal();
+  const { items } = useCart();
 
   return (
     <header className="pb-32 bg-gradient-to-r from-gray-900 via-gray-800 to-gray-700">
@@ -22,8 +23,16 @@ export default function HeaderProducts() {
             className="relative p-3 rounded-full bg-gray-800 hover:bg-gray-700 transition-all duration-300 shadow-xl focus:outline-none"
           >
             <MdOutlineShoppingCart className="text-3xl md:text-5xl text-white" />
-            <span className="absolute top-0 right-0 block h-2.5 w-2.5 rounded-full ring-2 ring-gray-900 bg-red-500 animate-ping"></span>
-            <span className="absolute top-0 right-0 block h-2.5 w-2.5 rounded-full bg-red-500"></span>
+            {items.length > 0 ? (
+              <>
+                <span className="absolute top-0 right-0 block h-2.5 w-2.5 rounded-full ring-2 ring-gray-900 bg-red-500 animate-ping"></span>
+                <span className="absolute top-0 right-0 block h-2.5 w-2.5 rounded-full bg-red-500"></span>
+              </>
+            ) : (
+              <>
+                <span className="absolute top-1 right-1 block h-1 w-1 rounded-full bg-green-500"></span>
+              </>
+            )}
           </button>
         </div>
       </div>
