35ed3b95d2163fceac931d82bf3a037b528f4816
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-09-24 14:59:39 -0300

    adjusting cypress to run in terminal and finishing project

--- cypress/e2e/page.cy.ts
+++ cypress/e2e/page.cy.ts
@@ -1,4 +1,12 @@
 describe("Home Page", () => {
+  it("should load all pages", () => {
+    cy.visit("http://localhost:3000");
+    cy.wait(1000);
+    cy.visit("http://localhost:3000/products");
+    cy.wait(1000);
+    cy.visit("http://localhost:3000/products/1");
+  });
+
   it("should load home page and show components", () => {
     cy.visit("http://localhost:3000");
     cy.get("button").contains(/See Products/i);
